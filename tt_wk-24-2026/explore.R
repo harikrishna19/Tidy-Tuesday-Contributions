84584ee827526e6d4b128e6247ba50c854307ad3
harikrishna19 <krishna19599@gmail.com>
2026-08-24 20:45:37 +0530

    Update initial file-explore.R

---
+++ tt_wk-24-2026/explore.R
@@ -0,0 +1,7 @@
+tuesdata <- tidytuesdayR::tt_load(2026, week = 34)
+
+country_lyrics <- tuesdata$country_lyrics
+top_all_writers <- tuesdata$top_all_writers
+top_primary_writers <- tuesdata$top_primary_writers
+top_producers <- tuesdata$top_producers
+
