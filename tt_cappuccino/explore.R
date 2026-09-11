37d24e378ad6e4a3f28c6dbbbbf8977adb320dd3
harikrishna19 <krishna19599@gmail.com>
2026-09-11 12:14:00 +0530

    Update initial-files read

--- tt_cappuccino/explore.R
+++ tt_cappuccino/explore.R
@@ -1,10 +1,11 @@
 # Using R
 # Option 1: tidytuesdayR R package 
 ## install.packages("tidytuesdayR")
+library(dplyr)
+
 
 tuesdata <- tidytuesdayR::tt_load('2026-09-08')
-## OR
-tuesdata <- tidytuesdayR::tt_load(2026, week = 36)
+
 
 cafe <- tuesdata$cafe
 cappuccino_index <- tuesdata$cappuccino_index
